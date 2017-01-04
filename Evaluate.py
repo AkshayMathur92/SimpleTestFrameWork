5475b5cf061e996f3815bb173e9ccd654324ad22
Akshay Mathur <amathur@adobe.com>
2017-01-04 13:45:38 +0530

    Proper parsing from csv to python values.

--- Evaluate.py
+++ Evaluate.py
@@ -1,7 +1,17 @@
 """Evaluate JSON based on given criteria"""
 import sys
 import getopt
+import csv
+import ast
+import re
 from importlib.machinery import SourceFileLoader
+
+def read_csv(file):
+    "readind csv rowwise"
+    with open(file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            yield row
 
 def main(argv):
     """Main """
@@ -21,6 +31,17 @@ def main(argv):
         elif opt in ("-f", "--functionfile"):
             functionfile = arg
     eval_module = SourceFileLoader("evaluate", functionfile).load_module()
-    eval_module.evaluate(**{'policy_name' : "hello", 'policy_description' : "world"})
+    number = re.compile("[0-9]+$")
+    boolean = re.compile("^([Tt][Rr][Uu][Ee]|[Ff][Aa][Ll][Ss][Ee])$")
+    for row in read_csv(csvfile):
+        new_row = {}
+        for key, value in row.items():
+            if isinstance(value, str) and (value.startswith('[') or value.startswith('{') or boolean.match(value)):
+                new_row[key] = ast.literal_eval(value)
+            elif isinstance(value, str) and number.match(value):
+                new_row[key] = int(value)
+            else:
+                new_row[key] = value
+        eval_module.evaluate(**new_row)
 
 main(sys.argv[1:])
